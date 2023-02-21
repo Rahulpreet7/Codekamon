5e78058d0b1cded16872604807e1d33fd73493c6
Shen Ruijun <ruijun2@ualberta.ca>
2023-02-20 19:50:52 -0700

    scan and scoring

--- code/app/src/main/java/com/example/codekamon/MainActivity.java
+++ code/app/src/main/java/com/example/codekamon/MainActivity.java
@@ -10,11 +10,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.google.zxing.*;
+//import com.google.zxing.activity.CodeUtils;
 
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.journeyapps.barcodescanner.CaptureActivity;
 
 import org.checkerframework.checker.nullness.qual.Nullable;
+import org.w3c.dom.Text;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,16 +29,28 @@ public class MainActivity extends AppCompatActivity {
     FirebaseFirestore firestore;
 
 
+    MessageDigest md = MessageDigest.getInstance("SHA-256");
+    TextView showScoreText;
+
+    public MainActivity() throws NoSuchAlgorithmException {
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //for algorithm testing
+        QRCode code = new QRCode("8a976a83da98f48f0b9f0");
+        int a = code.getScore();
+        Toast.makeText(MainActivity.this, Integer.toString(a), Toast.LENGTH_SHORT).show();
 
         //TextView messageText;
         //TextView messageFormat;
 
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-
+        showScoreText = findViewById(R.id.show_score_text);
         ImageView map = findViewById(R.id.map_icon);
         map.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,32 +65,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 IntentIntegrator intentIntegrator = new IntentIntegrator(MainActivity.this);
                 intentIntegrator.setPrompt("Scan a QR code");
-                intentIntegrator.setOrientationLocked(true);
+                intentIntegrator.setOrientationLocked(false);
                 intentIntegrator.initiateScan();
 
                 Toast.makeText(MainActivity.this, "Clicked 'add code'", Toast.LENGTH_SHORT).show();
 
+
             }
 
 
-            protected void onAcitivtyResult(int requestCode, int resultCode, @Nullable Intent data)
-            {
-                MainActivity.super.onActivityResult(requestCode,resultCode,data);
-                IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-                // if the intentResult is null then
-                // toast a message as "cancelled"
-                if (intentResult != null) {
-                    if (intentResult.getContents() == null) {
-                        Toast.makeText(getBaseContext(), "Cancelled", Toast.LENGTH_SHORT).show();
-                    } else {
-                        // if the intentResult is not null we'll set
-                        // the content and format of scan message
-                        Toast.makeText(getBaseContext(),intentResult.getContents() , Toast.LENGTH_SHORT).show();
-                        //messageText.setText(intentResult.getContents());
-                        //messageFormat.setText(intentResult.getFormatName());
-                    }
-                }
-            }
         });
 
         ImageView leaderboards = findViewById(R.id.leaderboards_icon);
@@ -99,5 +100,43 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    //received help from https://www.geeksforgeeks.org/how-to-read-qr-code-using-zxing-library-in-android/
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        MainActivity.super.onActivityResult(requestCode,resultCode,data);
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        // if the intentResult is null then
+        // toast a message as "cancelled"
+        if (intentResult != null) {
+            Bundle bundle = data.getExtras();
+            if (intentResult.getContents() == null) {
+                Toast.makeText(getBaseContext(), "Cancelled", Toast.LENGTH_SHORT).show();
+
+            } else {
+
+                //Toast.makeText(getBaseContext(),intentResult.getRawBytes().toString(), Toast.LENGTH_SHORT).show();
+                byte[] encrypted = md.digest(intentResult.getRawBytes());
+
+                StringBuilder sb = new StringBuilder();
+                for(byte b : encrypted)
+                {
+                    sb.append(String.format("%02x", b));
+                }
+                Toast.makeText(getBaseContext(),sb, Toast.LENGTH_SHORT).show();
+                QRCode scannedResult = new QRCode(sb.toString());
+                showScoreText.setText("Points Earned: " + scannedResult.getScore());
+
+
+
+
+
+                //messageText.setText(intentResult.getContents());
+                //messageFormat.setText(intentResult.getFormatName());
+            }
+        }
     }
 }
--- code/app/src/main/java/com/example/codekamon/QRCode.java
+++ code/app/src/main/java/com/example/codekamon/QRCode.java
@@ -11,6 +11,7 @@ public class QRCode {
     public QRCode(String content)
     {
         this.content = content;
+        this.score = calcScore();
     }
 
     public String getContent()
@@ -18,14 +19,57 @@ public class QRCode {
         return content;
     }
 
-    private int calcScore()
+    public int getScore()
     {
+        return score;
+    }
+
+    public int calcScore()
+    {
+        //needs further testing
+        int returner = 0;
+        char recChar;
+        int count = 0;
         for(int i = 0; i < content.length(); i++)
         {
 
+            if(i >= 1 && (content.charAt(i) == content.charAt(i - 1)))
+            {
+                count ++;
+            }else if(i >= 1)
+            {
+                if(content.charAt(i - 1) == '0')
+                {
+                    returner += Math.pow(20,count);
+                }
+                else if('1' <= content.charAt(i - 1) && content.charAt(i - 1) <= '9' && count > 0)
+                {
+                    returner += Math.pow((int)content.charAt(i - 1), count);
+
+                }
+                else if('a' <= content.charAt(i - 1) && content.charAt(i - 1) <= 'f' && count > 0)
+                {
+                    returner += Math.pow(content.charAt(i - 1) + 10 - 'a', count);
+                }
+                count = 0;
+            }
+            recChar = content.charAt(i);
+        }
+        if(content.charAt(content.length()-1) == '0')
+        {
+            returner += Math.pow(20,count);
+        }
+        else if('1' <= content.charAt(content.length()-1) && content.charAt(content.length()-1) <= '9' && count > 0)
+        {
+            returner += Math.pow((int)content.charAt(content.length()-1), count);
 
         }
-        return 0;
+        else if('a' <= content.charAt(content.length()-1) && content.charAt(content.length()-1) <= 'f' && count > 0)
+        {
+            returner += Math.pow(content.charAt(content.length()-1) + 10 - 'a', count);
+        }
+
+        return returner;
     }
 }
 
