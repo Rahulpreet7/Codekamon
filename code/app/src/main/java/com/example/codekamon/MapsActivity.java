00dd35e6ddfcaad0b8e234e70156e6ae19deaed2
Elisandro Cruz Martinez <cruzmart@ualberta.ca>
2023-03-07 21:26:57 -0700

    changed a couple of things

--- code/app/src/main/java/com/example/codekamon/MapsActivity.java
+++ code/app/src/main/java/com/example/codekamon/MapsActivity.java
@@ -6,11 +6,14 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -18,6 +21,8 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -26,6 +31,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
@@ -77,7 +83,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int PERMISSION_REQUEST_CODE = 101;
-    private static final float DEFAULT_ZOOM = 16;
     private static final float RADIUS = (float) 50.2;
 
     private LatLng currentLocation;
@@ -90,6 +95,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private ArrayList<DistancePlayerToTarget> targetsMarkers = new ArrayList<>();
     private DistanceListViewAdapter adapter;
 
+    private LocationManager locationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -98,6 +105,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         firebase = FirebaseFirestore.getInstance();
         collectionReference = firebase.collection("Test_Map");
 
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
         adapter = new DistanceListViewAdapter(this, targetsMarkers);
         ListView markersList = findViewById(R.id.listviewNear);
         markersList.setAdapter(adapter);
@@ -105,12 +114,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         markersList.setOnItemClickListener((AdapterView<?> parent, View view, int position, long id) -> {
             if (gMap != null) {
                 DistancePlayerToTarget t = (DistancePlayerToTarget) parent.getAdapter().getItem(position);
-                moveCamaraToCurrentLocation(t.getTarget());
+                moveCamara(t.getTarget());
             }
         });
 
         getLocationPermission();
     }
+
     /**
      * The method "getTargetsToGoogleMap" checks if thd database has gotten new Codekamons appearing in the map
      * , it will only allow to show and display the codekamons that are within the RADIUS of visibility of the player
@@ -121,19 +131,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             gMap.clear();
             findPlayerMarker();
 
-            if(firstTimeStartingUp) {
+            if (firstTimeStartingUp) {
                 Toast.makeText(MapsActivity.this, "Db Updated. Refresh map to see changes!", Toast.LENGTH_SHORT).show();
                 firstTimeStartingUp = false;
             }
 
             assert queryDocumentSnapshots != null;
-            for(QueryDocumentSnapshot doc: queryDocumentSnapshots) {
+            for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
                 if (doc.getData().get("lati") != null && doc.getData().get("long") != null) {
                     double lat = (double) doc.getData().get("lati"), lon = (double) doc.getData().get("long");
                     LatLng targetLatlng = new LatLng(lat, lon);
 
                     if (DistancePlayerToTarget.isDistanceInRadius(currentLocation, targetLatlng, RADIUS)) {
-                        if(doc.getData().get("name") != null) {
+                        if (doc.getData().get("name") != null) {
                             MarkerOptions i = new MarkerOptions().position(targetLatlng).title((String) doc.getData().get("name"));
                             targetsMarkers.add(
                                     new DistancePlayerToTarget(
@@ -150,17 +160,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
 
     }
+
     /**
      * The method "findPlauerMaker" adds a marker to the current location of the player.
      */
-    public void findPlayerMarker(){
+    public void findPlayerMarker() {
         MarkerOptions marker = new MarkerOptions().position(currentLocation).title("You").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
         gMap.addMarker(marker);
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap gMapp) {
-        Toast.makeText(this, "Success: Map loaded!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Map loaded!", Toast.LENGTH_SHORT).show();
         gMap = gMapp;
         if (mLocationPermissionGranted) {
             getDeviceLocation();
@@ -171,46 +182,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     /**
-     * The method "moveCamaraToCurrentLocation" zooms in at a marker or player location based on the DEFUALT_ZOOM and LngLon
+     * The method "moveCamara" zooms in at a marker or player location based on the DEFUALT_ZOOM and LngLon
      */
-    private void moveCamaraToCurrentLocation(LatLng latlng){
+    private void moveCamara(LatLng latlng) {
         gMap.animateCamera(CameraUpdateFactory.newLatLng(latlng));
-        gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, MapsActivity.DEFAULT_ZOOM));
+        gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 16));
     }
 
     /**
      * The method "initMap" this is used to add the map to the fragment "map" inside the "activity_maps" layout.
      */
-    private void initMap(){
+    private void initMap() {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         assert mapFragment != null;
         mapFragment.getMapAsync(MapsActivity.this);
     }
+
     /**
      * The method "getDeviceLocation" checks the permission to access the location and than zooms in to the location of the player
      */
-    public void getDeviceLocation(){
-        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        try{
-            if(mLocationPermissionGranted){
-                Task<Location> t = mFusedLocationProviderClient.getLastLocation();
-
-                t.addOnSuccessListener((Location location) -> {
-                    if(location != null){
-                        currentLocation = new LatLng(location.getLatitude(),location.getLongitude());
-                        Toast.makeText(this, ""+currentLocation.latitude + "," + currentLocation.longitude, Toast.LENGTH_SHORT).show();
-
-                        findPlayerMarker();
-
-                        moveCamaraToCurrentLocation(currentLocation);
-                        getTargetsToGoogleMap();
-                    }
-                });
+    @SuppressLint("MissingPermission")
+    public void getDeviceLocation() {
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MapsActivity.this);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 0, new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                findPlayerMarker();
+                moveCamara(currentLocation);
             }
-
-        }catch (SecurityException s){
-            Toast.makeText(this, "ERROR: current location not found", Toast.LENGTH_LONG).show();
-        }
+        });
     }
     /**
      * The method "getLocationPermission" checks and obtains the permission to access the location for the device this app is running
@@ -218,10 +219,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private void getLocationPermission(){
         String[] permissions = {Manifest.permission.ACCESS_COARSE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION};
+
         if(ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
             if(ContextCompat.checkSelfPermission(this.getApplicationContext(), COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                 mLocationPermissionGranted = true;
-                initMap();
             }else{
                 ActivityCompat.requestPermissions(this, permissions
                         ,PERMISSION_REQUEST_CODE);
@@ -238,6 +239,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         mLocationPermissionGranted = false;
+
         if (requestCode == PERMISSION_REQUEST_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 for (int grantResult : grantResults)
@@ -245,6 +247,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         mLocationPermissionGranted = false;
                         return;
                     }
+                //At this point permission has been granted
                 mLocationPermissionGranted = true;
                 initMap();
             }
