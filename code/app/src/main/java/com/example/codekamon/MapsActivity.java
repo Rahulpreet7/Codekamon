bc74867ca6dc4189735de3fe5c3fdbb70c3f9258
Elisandro Cruz Martinez <cruzmart@ualberta.ca>
2023-03-14 19:50:04 -0600

    changed map from GPS -> Fused

--- code/app/src/main/java/com/example/codekamon/MapsActivity.java
+++ code/app/src/main/java/com/example/codekamon/MapsActivity.java
@@ -79,7 +79,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private LocationManager locationManager;
     private LocationListener locationListener;
     private Button bck;
-
     private boolean showDatabaseUpdated = false;
 
     @Override
@@ -107,7 +106,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 qr_code_markers.clear();
                 if(showDatabaseUpdated)
                     Toast.makeText(MapsActivity.this, "Change: Updated Codes In List", Toast.LENGTH_SHORT).show();
-                showDatabaseUpdated = true;
                 assert queryDocumentSnapshots != null;
                 for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
                     MarkerOptions item = new MarkerOptions()
@@ -139,34 +137,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Toast.makeText(this, "Map loaded!", Toast.LENGTH_SHORT).show();
         gMap = googleMap;
 
+
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(@NonNull Location location) {
-                currentPosition = new LatLng(location.getLatitude(), location.getLongitude());
+                    currentPosition = new LatLng(location.getLatitude(), location.getLongitude());
 
-                gMap.clear();
-                qr_code_items.clear();
+                    gMap.clear();
+                    qr_code_items.clear();
 
-                addPlayerMarker();
+                    addPlayerMarker();
 
-                Toast.makeText(MapsActivity.this, "Location Changed!", Toast.LENGTH_SHORT).show();
+                    move_camara(currentPosition);
+                    Toast.makeText(MapsActivity.this, "Location Changed!", Toast.LENGTH_SHORT).show();
 
-                for (MarkerOptions marker : qr_code_markers) {
-                    if(isThisIsTheSameLocation(currentPosition, marker.getPosition())){
-                        if(DistancePlayerToTarget.pointsAreWithinRadius(currentPosition, marker.getPosition(), radius)){
-                            qr_code_items.add(
-                                    new DistancePlayerToTarget(marker.getTitle(), currentPosition, marker.getPosition())
-                            );
-                            gMap.addMarker(marker);
+                    for (MarkerOptions marker : qr_code_markers) {
+                        if (isThisIsTheSameLocation(currentPosition, marker.getPosition())) {
+                            if (DistancePlayerToTarget.pointsAreWithinRadius(currentPosition, marker.getPosition(), radius)) {
+                                qr_code_items.add(
+                                        new DistancePlayerToTarget(marker.getTitle(), currentPosition, marker.getPosition())
+                                );
+                                gMap.addMarker(marker);
+                            }
                         }
                     }
-                }
-                adapter.notifyDataSetChanged();
+                    adapter.notifyDataSetChanged();
             }
         };
 
         getLocationPermission();
+
     }
 
     /**
@@ -179,16 +181,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
                 if (ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION)
                         != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getBaseContext(),
-                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {return;}
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
 
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 100, locationListener);
-
-
-                // This is called when we first get the location. Never called again
-                Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                currentPosition = new LatLng(lastLocation.getLatitude(),lastLocation.getLongitude());
-                addPlayerMarker();
-                move_camara(currentPosition);
+                locationManager.requestLocationUpdates(LocationManager.FUSED_PROVIDER,1000, 0, locationListener);
             }
             @Override
             public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {}
